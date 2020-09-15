ea39b7135cb56ccb8c1b080f6744335855039292
Tobias Kattanek <tkattanek00@gmail.com>
2020-09-15 20:21:17 +0200

    Inventory System
    started adding inventory system => very bad approach, its all combined with redundant code and prefabs and stuff, this can be Interfaced Implemented

---
+++ Assets/Scripts/Inventory/Pickup.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+
+    private Inventory inventory;
+
+    public GameObject itemButtonPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("PLAYER").GetComponent<Inventory>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("PLAYER"))
+        {
+            for (int i = 0; i < inventory.slots.Length; i++)
+            {
+                if (inventory.isFull[i] == false)
+                {
+                    // ITEM CAN BE ADDED
+                    inventory.isFull[i] = true;
+                    Instantiate(itemButtonPrefab, inventory.slots[i].transform, false);
+                    Destroy(gameObject);
+                    break;
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
